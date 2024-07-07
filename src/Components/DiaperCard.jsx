e4453cb74348ada344d2252b951047f6e93d29f7
[Jackson Bond] <jacksonbond11@yahoo.com>
2024-07-06 19:49:54 -0500

    updated server ip

--- src/Components/DiaperCard.jsx
+++ src/Components/DiaperCard.jsx
@@ -1,7 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const DiaperCard = ({ imgSrc, imgHeight = 'h-36', imgWidth = 'w-1/2', title, quantityProp, startingTotal }) => {
-  const [name, setName] = useState('');
+const DiaperCard = ({
+  imgSrc,
+  imgHeight = "h-36",
+  imgWidth = "w-1/2",
+  title,
+  quantityProp,
+  startingTotal,
+}) => {
+  const [name, setName] = useState("");
   const [quantity, setQuantity] = useState(0);
   const [amountNeeded, setAmountNeeded] = useState(startingTotal);
 
@@ -15,12 +22,14 @@ const DiaperCard = ({ imgSrc, imgHeight = 'h-36', imgWidth = 'w-1/2', title, qua
 
   const fetchCurrentQuantity = async () => {
     try {
-      const response = await fetch(`http://localhost:3001/api/diapers/${title}/quantity`);
+      const response = await fetch(
+        `http://24.144.94.207:3001/api/diapers/${title}/quantity`
+      );
       const result = await response.json();
       const totalBought = result.total_bought || 0;
       setAmountNeeded(startingTotal - totalBought);
     } catch (error) {
-      console.error('Error fetching current quantity:', error);
+      console.error("Error fetching current quantity:", error);
     }
   };
 
@@ -29,42 +38,52 @@ const DiaperCard = ({ imgSrc, imgHeight = 'h-36', imgWidth = 'w-1/2', title, qua
   }, []);
 
   const handleSubmit = async () => {
-    const endpoint = quantityProp === "1" ? 'http://localhost:3001/api/diapers' : 'http://localhost:3001/api/beer';
-    const data = quantityProp === "1" ? { name, quantity: parseInt(quantity, 10), title } : { name, title };
+    const endpoint =
+      quantityProp === "1"
+        ? "http://24.144.94.207:3001/api/diapers"
+        : "http://24.144.94.207:3001/api/beer";
+    const data =
+      quantityProp === "1"
+        ? { name, quantity: parseInt(quantity, 10), title }
+        : { name, title };
 
     try {
       const response = await fetch(endpoint, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
       });
 
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
 
       const result = await response.json();
       console.log(result);
 
       // Clear the form after submission
-      setName('');
+      setName("");
       setQuantity(0);
 
       // Refresh the current quantity
       fetchCurrentQuantity();
 
-      alert('Submission successful!');
+      alert("Submission successful!");
     } catch (error) {
-      console.error('There was a problem with the submission:', error);
-      alert('Submission failed.');
+      console.error("There was a problem with the submission:", error);
+      alert("Submission failed.");
     }
   };
 
   return (
     <div className="max-w-xs mx-auto bg-white shadow-md rounded-lg overflow-hidden border-2 border-gray-300">
-      <img src={imgSrc} alt="Diaper" className={`${imgWidth} ${imgHeight} object-cover`} />
+      <img
+        src={imgSrc}
+        alt="Diaper"
+        className={`${imgWidth} ${imgHeight} object-cover`}
+      />
       <div className="p-2">
         <h2 className="text-lg font-bold mb-2">{title}</h2>
         {quantityProp === "1" && (

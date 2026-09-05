6e37e83fe17395d88d4d24bbc4ac583d0850a2d0
Jackson Bond <jacksonbond11@yahoo.com>
2026-09-04 21:50:24 -0500

    stuff

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
+import App from './App';
 import { render, screen } from '@testing-library/react';
 jest.mock('react-markdown', () => () => null);
 jest.mock('@vercel/analytics/react', () => ({ Analytics: () => null }), { virtual: true });
 jest.mock('./Components/Neo4jGraph.jsx', () => () => null);
-import App from './App';
 
 test('renders the portfolio homepage', () => {
   render(<App />);
